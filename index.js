690e628bbd9478e085c1fe370e2f340fb952b8c7
Glenn Nguyo <glennnguyo1000@gmail.com>
2022-06-08 16:09:32 +0300

    master

---
+++ index.js
@@ -0,0 +1,12 @@
+//const { spy } = require("chai");
+
+
+function returnsANamedFunction(){
+ return returnsANamedFunction;
+}
+function  receivesAFunction(callback){
+   callback();
+}
+var anonymous =function  returnsAnAnonymousFunction(){
+    return anonymous();
+}
